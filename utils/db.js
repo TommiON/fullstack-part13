b029bdc1175565eaae35a8f2ea2ca3f55eee0c88
TommiON <tommi.niittymies@gmail.com>
2022-05-30 22:45:33 +0300

    Exercises 13.17, 13.18

--- utils/db.js
+++ utils/db.js
@@ -1,5 +1,6 @@
 const Sequelize = require('sequelize')
 const { DATABASE_URL } = require('./config')
+const { Umzug, SequelizeStorage } = require('umzug')
 console.log('DATABASE_URL: ', DATABASE_URL)
 
 const sequelize = new Sequelize(DATABASE_URL, {
@@ -11,12 +12,31 @@ const sequelize = new Sequelize(DATABASE_URL, {
     },
 });
 
+const runMigrations = async () => {
+    const migrator = new Umzug({
+        migrations: {
+          glob: 'migrations/*.js',
+        },
+        storage: new SequelizeStorage({ sequelize, tableName: 'migrations' }),
+        context: sequelize.getQueryInterface(),
+        logger: console,
+      })
+    
+    const migrations = await migrator.up()
+    
+    console.log('Migrations up to date', {
+        files: migrations.map((mig) => mig.name),
+    })
+}    
+
+
 const connectToDataBase = async () => {
     try {
         await sequelize.authenticate()
+        await runMigrations()
         console.log('muodostettu yhteys tietokantaan')
     } catch (error) {
-        console.log('VIRHE: ei yhteyttä tietokantaan')
+        console.log('VIRHE: ei yhteyttä tietokantaan', error)
         return process.exit(1)
     }
 
--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -1,4 +1,7 @@
 const errorHandler = (error, request, response, next) => {
+    if (error.name === 'SequelizeValidationError' && (error.message.includes('Validation min on year failed') || error.message.includes('Validation max on year failed'))) {
+        response.status(400).send( { error: 'Vuosi saa olla välillä 1991 - kuluva vuosi '})
+    }
     if (error.name === 'SequelizeValidationError') {
         response.status(400).send({ error: 'Käyttäjätunnuksen pitää olla validi sähköpostisoite' })
     }
