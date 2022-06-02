d9c455c43669db3fde4f6436758ec31a10283159
TommiON <tommi.niittymies@gmail.com>
2022-06-02 20:24:08 +0300

    Exercise 13.22

--- controllers/readinglists.js
+++ controllers/readinglists.js
@@ -1,11 +1,28 @@
 const router = require('express').Router()
-const { Readinglist } = require('../models')
+const { Readinglist, User } = require('../models')
 const tokenExtractor = require('../utils/tokenExtractor')
 
 router.post('/', tokenExtractor, async (req, res, next) => {
     try {
         const thingToRead = await Readinglist.create(req.body)
         res.json(thingToRead)
+    } catch(error) {
+        next(error)
+    }
+})
+
+router.put('/:id', tokenExtractor, async (req, res, next) => {
+    try {
+        const target = await Readinglist.update(
+            {read: req.body.read},
+            {where: {id: req.params.id, user_id: req.decodedToken.id }}
+        )
+
+        if((target[0]) === 0) {
+            res.send('Blogia ei löydy tai ei ole sinun blogisi')
+        }
+
+        res.status(200).send('luettu!')
     } catch(error) {
         next(error)
     }
